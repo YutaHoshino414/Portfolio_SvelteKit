4182498d81a3df7120ac78f439be2384bf6364fa
Yuta Hoshino <y.hoshino25@gmail.com>
2022-04-10 00:29:19 +0900

    Add background & glass component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,39 @@
+<script>
+  import logo from "../assets/user.jpg"
+</script>
+
+<div class="nav">
+  <div class="user">
+    <img src={logo} alt="user" />
+    <p>Y.Hoshino</p>
+  </div>
+  <p>Home</p>
+  <p>About</p>
+  <p>Works</p>
+  <p>etc</p>
+</div>
+
+<style>
+  .nav {
+    flex:1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    text-align: center;
+    background: linear-gradient(
+      to right bottom,
+      rgba(255, 255, 255, 0.6),
+        rgba(255, 255, 255, 0.1)
+    );
+    border-radius: 2rem;
+  }
+
+  .user img {
+    width: 160px;
+    height:160px;
+    border-radius: 50%;
+    border: 5px solid #fff;
+    object-fit: cover;  /* この一行を追加するだけ！ */
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,94 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Navbar from "../components/Navbar.svelte";
+  import global from  "../global.css"
+</script>
+
+<div class="main">
+  <div class="glass"> 
+    <Navbar />
+    <div class="content">dd</div>
+  </div>
+  <div class="circle1"></div>
+  <div class="circle2"></div>
+  <div class="circle3"></div>
+</div>
+
+
+<style>
+.main{
+  min-height: 100vh;
+  background-color: #FAFFB7; 
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fde285'/%3E%3Cstop offset='1' stop-color='%23fde285' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%235cff72'/%3E%3Cstop offset='1' stop-color='%235cff72' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%234cfc2b'/%3E%3Cstop offset='1' stop-color='%234cfc2b' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23FAFFB7'/%3E%3Cstop offset='1' stop-color='%23FAFFB7' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23FAA454'/%3E%3Cstop offset='1' stop-color='%23FAA454' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230CF'/%3E%3Cstop offset='1' stop-color='%230CF' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.glass {
+  background: white;
+  min-height: 80vh;
+  width:80%;
+  background: linear-gradient(
+    to right bottom, 
+  rgba(255, 255, 255, 0.7),
+  rgba(255, 255, 255, 0.2)
+  );
+  border-radius: 2rem;
+  backdrop-filter:blur(1rem);
+  z-index: 2;
+  display:flex;
+}
+
+.content {
+  flex:3;
+}
+
+
+.circle1,
+.circle2,
+.circle3 {
+  background: white;
+  background: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.9),
+      rgba(255, 255, 255, 0.3)
+  );
+  position: absolute;
+  border-radius: 50%;
+}
+
+.circle1{
+  top: 5%;
+  right: 15%;
+  height: 15rem;
+  width: 15rem;
+}
+
+.circle2{
+  bottom: 3%;
+  left: 35%;
+  height: 10rem;
+  width: 10rem;
+  animation: zoomInOut 8s linear infinite;
+  background-color: rgb(255, 157, 45);
+}
+
+.circle3{
+  top: 35%;
+  left: 30%;
+  height: 5rem;
+  width: 5rem;
+  background-color: rgb(50, 122, 255);
+}
+/* @keyframes zoomInOut{
+  0%, 100%{
+    transform: scale(0.5);
+  }
+  50% {
+    transform: scale(1)
+  }
+} */
+
+</style>
